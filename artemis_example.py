af5e704f4885de83e278c83211bd5936d1294073
jjbeck <57514232+jjbeck@users.noreply.github.com>
2020-04-19 13:07:56 -0400

    Initial main script and example

---
+++ artemis_example.py
@@ -0,0 +1,8 @@
+from artemis_v1 import show_prediction
+
+a = show_prediction('/home/jordan/Desktop/nihgpppipe/Annot', '/home/jordan/Desktop/nihgpppipe/Annot',
+                    '/home/jordan/Desktop/nihgpppipe/Annot')
+a.show_intro()
+a.load_video_organize_dir()
+last_frame = a.determine_last_frame()
+a.loop_video(last_frame)
