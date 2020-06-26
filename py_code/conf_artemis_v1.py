f1ae39ae453aa3faeec0cdbbbe49183312b81197
Sami Bou Ghanem <samibghanem@gmail.com>
2020-06-26 00:02:08 -0400

    Changed check_load_csv

--- py_code/conf_artemis_v1.py
+++ py_code/conf_artemis_v1.py
@@ -8,12 +8,17 @@ from tkinter.filedialog import askopenfilename, askdirectory
 from tkinter import simpledialog
 import pandas as pd
 import re
+import yaml
+
 
 class conf_matrix_artemis():
 
     def __init__(self, path_to_video, path_to_csv):
         self.path_to_video = path_to_video
         self.path_to_csv = path_to_csv
+        with open('../Annot/config.yaml') as config:
+            data = yaml.load(config, Loader=yaml.FullLoader)
+            self.boot_round = data['Boot Round']
         self.BEHAVIOR_LABELS = {
             0: "drink",
             2: "groom",
@@ -31,53 +36,73 @@ class conf_matrix_artemis():
         self.csv_results = {}
         self.num_right = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
         self.total_analyzed = {0: 0, 1: 0, 2: 0, 3: 0, 4: 0, 5: 0, 6: 0, 7: 0, 8: 0}
-        self.accuracy_annotations = np.zeros((9,9))
-        self.confusion_matrix = np.zeros((9,9))
-        self.nooo = np.empty((1,9))
-        self.nooo[:]= np.NaN
+        self.accuracy_annotations = np.zeros((9, 9))
+        self.confusion_matrix = np.zeros((9, 9))
+        self.nooo = np.empty((1, 9))
+        self.nooo[:] = np.NaN
         self.csv_name = []
         self.pickle_name = []
-        self.accuracy_annotations = np.zeros(shape=(9,9))
-        self.confusion_matrix = np.zeros(shape=(9,9))
+        self.accuracy_annotations = np.zeros(shape=(9, 9))
+        self.confusion_matrix = np.zeros(shape=(9, 9))
 
     def check_load_csv(self):
         root = tk.Tk()
         root.withdraw()
-        self.analyze_csv= []
+        self.analyze_csv = []
         self.analyze_pickle = []
         i = 0
-        self.boot_round = simpledialog.askstring(title="Test",
-                                                 prompt="What bootstrap round is this? (0,2, etc; or type all")
-        self.csv_file = askdirectory(initialdir="/home/jordan/Desktop/nihgpppipe/Annot/csv_not_done",
-                                        title="Select CSV folder")  # show an "Open" dialog box and return the path to the selected file
-        self.pickle_path = askdirectory(initialdir="/home/jordan/Desktop/nihgpppipe/Annot/pickle_files",title="Select pickle folder")
+
+        # First go through "/Annot/pickle_files/test" and check if file in this directory is in the directory "/Annot/csv_not_done/" .
+        # If file is, append picked file name to self.pickle_name and csv file name to self.csv_name
+        self.csv_file = askdirectory(initialdir="Annot/csv_not_done",
+                                     title="Select CSV folder")  # show an "Open" dialog box and return the path to the selected file
+        self.pickle_path = askdirectory(initialdir="Annot/pickle_files", title="Select pickle folder")
+
+        # TODO: First iterate through pkl test files. If there is a matching csv file, append both to respectile
+        #  list. This will guarantee that element at each index in list corresponds to respective element at index in
+        #  other list.
+        set_of_csv = set()
+        set_of_pkl = set()
+
         try:
-            for csvfile in glob.glob(self.csv_file + '/*.csv'):
-                self.csv_name.append(csvfile[csvfile.rfind('/')+1:])
+            for csv in glob.glob(self.csv_file + '/*.csv'):
+                # We clean the csv name to find just the file name:
+                #   csv_not_done/abc.csv --> abc
+                file_name = csv.replace(self.csv_file, '').replace('.csv', '')
+                set_of_csv.add(file_name)
         except:
             print('No CSV file in directory. Transfer some and run again')
 
+        # Suffix for rebuilding pickle name.
+        pickle_suffix = ''
         try:
             for picklefile in glob.glob(self.pickle_path + '/*.p'):
-                if self.boot_round == picklefile[-3]:
-                    self.pickle_name.append(picklefile[picklefile.rfind('/')+1:])
-                elif self.boot_round.lower() == 'all':
-                    self.pickle_name.append(picklefile[picklefile.rfind('/')+1:])
-
+                file_name = picklefile.replace(self.pickle_path, '')
+                # Saves the suffix past the last '_' to rebuild pickle name later.
+                pickle_suffix = file_name[file_name.rfind('_'):]
+                # Finds the last occurence of '_', and takes the string up to that.
+                file_name = file_name[:file_name.rfind('_')]
+                # test/abc_test.p --> abc
+                set_of_pkl.add(file_name)
         except:
             print('No Pickle file in directory. Transfer some and run again')
-        for file in self.pickle_name:
-            for csv in self.csv_name:
-                if re.match(file[:file.rfind('_')]+".*",csv):
-                    self.analyze_pickle.append(file)
+
+        common_files = list(set_of_pkl.intersection(set_of_csv))
+        # We rebuild list of csv and pickles from this intersection.
+        for file in common_files:
+            csv_name_rebuilt = self.csv_file + file + '.csv'
+            self.csv_name.append(csv_name_rebuilt)
+            pickle_name_rebuilt = self.pickle_path + file + pickle_suffix
+            self.pickle_name.append(pickle_name_rebuilt)
+
         print(self.analyze_pickle)
 
     def analyze_csv_pickle(self):
         for file in self.analyze_pickle:
-            csv_data = pd.read_csv(self.csv_file+'/{}'.format(file[:file.rfind('_')])+'.csv', names=[0,1])
-            annotations = pd.read_pickle(self.pickle_path+"/"+file)
+            csv_data = pd.read_csv(self.csv_file + '/{}'.format(file[:file.rfind('_')]) + '.csv', names=[0, 1])
+            annotations = pd.read_pickle(self.pickle_path + "/" + file)
             start_frame = annotations['frame'][1]
-            for index,row in annotations.iterrows():
+            for index, row in annotations.iterrows():
                 end_frame = (row['frame'])
                 if end_frame - start_frame == 10:
                     ten_frames_annotations = []  # make list to store last 10 annotations and predictions
@@ -114,7 +139,10 @@ class conf_matrix_artemis():
         plt.xlabel('Predictions')
         plt.show()
 
+
+
+
 if __name__ == "__main__":
-    a = conf_matrix_artemis('/home/jordan/Desktop/nihgpppipe/Annot','/home/jordan/Desktop/nihgpppipe/Annot')
+    a = conf_matrix_artemis('/home/jordan/Desktop/nihgpppipe/Annot', '/home/jordan/Desktop/nihgpppipe/Annot')
     a.check_load_csv()
     a.analyze_csv_pickle()
